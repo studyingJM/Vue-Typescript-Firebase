536220ed0497bac4ac84ea14e15515e5e210dbeb
jimin <kjimin2123@naver.com>
2021-07-30 18:37:08 +0900

    converter in func

---
+++ functions/src/plugins/firebase.ts
@@ -0,0 +1,11 @@
+import * as admin from 'firebase-admin'
+
+import serviceAccount from '../key.json'
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+})
+
+export const db = admin.firestore()
+
+export default admin
